d467e92e3df8af448a327d714005dcddb5a5363c
maglada <vitaliioperrenko@gmail.com>
2025-10-28 19:34:08 +0200

    Lab3

--- lab1/Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
+++ lab1/Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb335723d6043f037caec28b8c8a7d59eec16a5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+498b2dca5352ea08bd58dbd0ba160c0a5eba0e9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
